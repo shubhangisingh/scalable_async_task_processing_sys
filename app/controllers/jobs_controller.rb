b4891a53d2f626b88b01a67a57968a1830fd10e0
shubhangisingh <shubhangi.mca09@gmail.com>
2026-07-06 13:54:24 +0530

    add blank

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,6 +5,7 @@ class JobsController < ApplicationController
     render json: { id: job.id, status: job.status }, status: :accepted
   end
 
+  
   def show
     job = Job.find(params[:id])
     render json: job
